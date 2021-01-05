03f6c475f2a1abd40c43118055d127213d8a5496
neha043 <neha043@gmail.com>
2021-01-05 20:50:45 +0530

    Added Constraint

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint= Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constraintlog;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +38,19 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    constraintlog= new Log(200,100,50,PI/2)
+
+
+    var options={
+            bodyA: bird.body,
+            bodyB: constraintlog.body,
+            stiffness: 0.7,
+            length: 50
+
+    }
+
+    chain = Constraint.create(options) ;
+    World.add(world,chain);
 
 }
 
@@ -61,4 +77,9 @@ function draw(){
 
     bird.display();
     platform.display();
+    constraintlog.display();
+     
+    strokeWeight(6);
+    stroke("red")
+    line(bird.body.position.x , bird.body.position.y ,constraintlog.body.position.x, constraintlog.body.position.y);
 }
